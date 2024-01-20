eb5451775dd264f1d431e37233bb396281d9c3c1
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2024-01-20 18:01:18 +0530

    features done

--- portfolio/src/components/features/features.jsx
+++ portfolio/src/components/features/features.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-
+import Title from "../../layouts/Title";
+import Card from "../../layouts/Card";
+import { featuresData } from "./featuredata";
 const Features = () => {
+  // console.log(featuresData)
   return (
-    <section id="features" className="w-full h-[500px] flex items-center border-b-[1px] border-b-black">
-      features
+    <section id="features" className="w-[90%] mx-auto py-20 border-b-[1px] border-b-white">
+      <Title title="Features" desc="What i do"/>
+      <div className="grid grid-cols-3 gap-20">
+        {
+          featuresData.map((featureData)=>{
+            return <Card key={featureData.id} icon={featureData.icon} title={featureData.title} des={featureData.des}/>
+          })
+        }
+      </div>
     </section>
   );
 };
